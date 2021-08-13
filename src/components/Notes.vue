fad60ef366724a7948b253319a01983f982f50ee
KislevBecker <kisbeckerchavez@gmail.com>
2021-08-13 17:49:04 -0600

    ultima subida

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="container container-fluid">
-    <h1 class="display-4 text-center">Listado de notas</h1>
-    <hr />
-
+<div>
+<nav class="navbar sticky-top navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1 text-white">Notes Reminder</span>
+  </div>
+</nav>
+  <div class="container container-fluid mycontainer-background">
     <div v-if="loading" class="text-center">
       <div class="spinner-border text-danger" role="status">
         <span class="visually-hidden">Loading...</span>
       </div>
     </div>
     <div class="row">
-      <div class="col-lg-8 offset-lg-3">
-        <div class="card mt-4">
-          <div class="card-body">
+      <div class="col-lg-12">
+        <div class="mt-4">
+          <div >
+            <div class="text-right">
             <!-- Trigger the modal with a button -->
             <button
               type="button"
-              class="btn btn-primary"
+              class="btn btn-default btn-lg mt-2 mybutton float-end"
               data-bs-toggle="modal"
               data-bs-target="#exampleModal"
             >
               <i class="fas fa-plus-circle"></i> Add a new note
             </button>
-
+</div>
             <!-- Modal -->
             <div
               class="modal fade"
@@ -34,7 +38,7 @@
                 <!-- Modal content-->
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h4 class="modal-title">Create a new note</h4>
+                    <h4 class="modal-title">{{modalTitle}}</h4>
                     <button
                       type="button"
                       class="btn-close"
@@ -67,12 +71,12 @@
                     <div class="form-floating mb-3">
                       <input
                         type="datetime-local"
-                        id="expiration_date"
+                        id="expirationDate"
                         class="form-control"
-                        v-model="expiration_date"
+                        v-model="expirationDate"
                         placeholder="Enter a date"
                       />
-                      <label class="control-label" for="expiration_date"
+                      <label class="control-label" for="expirationDate"
                         >Expiration date:</label
                       >
                     </div>
@@ -106,37 +110,39 @@
         </div>
       </div>
     </div>
-    <div class="row mt-4">
+    <div class="row mt-2">
       <div v-if="!loading"></div>
       <div v-for="(nota, index) in listaNotes" class="col-md-3" :key="index">
-        <div class="card">
+        <div class="card mycards mb-5  cursor-pointer">
           <div class="card-header">
-            {{ nota.expiration_date }}
+           <small v-bind:class="[
+                (nota.expirationDate > nota.creationDate) ? 'text-black' : 'text-danger',
+              ]" data-toggle="tooltip" data-placement="top" title="Review this note"> {{ nota.expirationDate }}</small>
           </div>
           <div class="card-body">
-            <p><i class="fas fa-star"></i> {{ nota.title }}</p>
+            <p><i class="fas fa-star text-warning"></i> {{ nota.title }}</p>
             <hr />
             <p>
               Description: <small>{{ nota.descript }}</small>
             </p>
             <p>
-              Creation date: <small>{{ nota.creation_date }}</small>
+              Creation date: <small>{{ nota.creationDate }}</small>
             </p>
-            <!-- @*<p>{{nota.priority}}</p>*@ -->
           </div>
           <div class="card-footer justify-content-between">
             <button
               type="button"
-              class="btn btn-default btn-sm"
+              class="btn btn-default btn-sm text-info"
               data-bs-toggle="modal"
               data-bs-target="#exampleModal"
-             v-on:click="updateNote(nota.id)"
+             v-on:click="getDataUpdate(nota, nota.id)"
             >
+                         <!-- v-on:click="updateNote(nota, nota.id)" -->
                <i class="fas fa-pencil-alt"></i> Edit
             </button>
             <button
               type="button"
-              class="btn btn-default btn-sm"
+              class="btn btn-default btn-sm text-danger"
               v-on:click="deleteNote(nota.id)"
             >
               <i class="fas fa-trash-alt"></i> Delete
@@ -146,6 +152,11 @@
       </div>
     </div>
   </div>
+  <footer class="footer">
+    <p>&copy; 2021 </p>
+  </footer>
+</div>
+
 </template>
 
 <script>
@@ -159,19 +170,25 @@ export default {
     return {
       nota: "",
       listaNotes: [],
-      modalTitle:"",
+      modalTitle:"Add Note",
       loading: false,
+      id : 0,
     };
   },
   methods: {
     addNote() {
-      this.modalTitle="Add Note";
       const nota = {
         title: this.title,
         descript: this.descript,
-        creation_date: new Date(),
-        expiration_date: this.expiration_date,
+        creationDate: new Date(),
+        expirationDate: this.expirationDate,
       };
+      if(this.id)
+      {
+        this.updateNote(nota, this.id);
+      }
+      else{
+      
       this.loading = true;
       console.log(nota);
       axios
@@ -180,16 +197,26 @@ export default {
           console.log(response);
           this.loading = false;
           this.getNotes();
+          this.removeDataSubmit()
         })
         .catch((error) => {
           this.loading = false;
           console.error(error);
         });
       this.nota = "";
+      }
     },
-    updateNote(nota, id) {
-      this.modalTitle="Edit Note";
+    getDataUpdate(nota, id){
+        this.modalTitle="Edit Note";
+        this.id= id;
+        this.title= nota.title;
+        this.descript=nota.descript;
+        this.expirationDate=nota.expirationDate;
+
+    },
+    updateNote(id, nota) {       
       this.loading= true;
+      console.log(id);
       axios.put(URL + id, nota)
         .then((response) => {
           console.log(response);
@@ -228,6 +255,11 @@ export default {
           console.log(error);
           this.loading = false;
         })
+    },
+    removeDataSubmit(){
+       this.title= "";
+        this.descript="";
+        this.expirationDate="";
     }
   },
   created: function () {
@@ -237,4 +269,37 @@ export default {
 </script>
 
 <style scoped>
+.navbar, .navbar-light, .bg-light{
+    background-color: rgb(180,1,23) !important;
+    color: white !important;
+}
+
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgb(180,1,23);
+    color: white;
+    text-align: center;
+}
+
+.mycontainer-background {
+    background: rgb(0,0,0);
+    background: linear-gradient(125deg, rgba(0,0,0,1) 3%, rgba(116,116,116,1) 22%, rgba(193,193,193,1) 46%, rgba(196,200,201,1) 100%);
+}
+
+.mybutton:hover{
+/* font-size: 1.1rem; */
+font-weight: bold;
+color: white;
+}
+
+.mycards{
+  height: 20rem !important;
+}
+
+.cursor-pointer{
+  cursor: pointer;
+}
 </style>
